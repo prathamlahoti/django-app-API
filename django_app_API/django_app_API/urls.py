9af84ba83d36b0ce34c6a5034ebd68ea0accf733
Alex Cold <malxao666@gmail.com>
2019-04-03 18:24:02 +0300

    API schema added.

--- django_app_API/django_app_API/urls.py
+++ django_app_API/django_app_API/urls.py
@@ -4,13 +4,19 @@ from django.conf import settings
 from posts.urls import router as posts_router
 from categories.urls import router as categories_router
 from tags.urls import router as tags_router
+from rest_framework.schemas import get_schema_view
+
+
+schema_view = get_schema_view(title='Django app API')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('schema/', schema_view),
     path('', include(posts_router.urls)),
-    path('categories/', include(categories_router.urls)),
-    path('tags/', include(tags_router.urls)),
+    path('', include(categories_router.urls)),
+    path('', include(tags_router.urls)),
 ]
 
 if settings.DEBUG:
